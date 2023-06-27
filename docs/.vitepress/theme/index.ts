e3abad69eb6387c4fc03f19154a24f6df6b0e6b4
linyuanyuan <herryleehom@126.com>
2023-06-27 23:30:50 +0800

    feat: updated related logic

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,18 @@
-import BluephaPlus from 'bluepha-plus'
+// import BluephaPlus from 'bluepha-plus'
+import { BlButton } from "../../../packages/components/button"
 import DefaultTheme from 'vitepress/theme'
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
-
+import { useComponents } from './useComponents'
 // import VPApp, { NotFound, globals } from '../vitepress'
-import 'uno.css'
-import './style.css'
+// import 'uno.css'
+// import './style.css'
 import type { Theme } from 'vitepress'
 
 export default {
   ...DefaultTheme,
   enhanceApp: ({ app }) => {
-    app.use(BluephaPlus)
-
+    app.use("bl-button", BlButton)
+    useComponents(app)
     // globals.forEach(([name, Comp]) => {
     //   app.component(name, Comp)
     // })
